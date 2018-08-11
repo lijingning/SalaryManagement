2398241586d320db733dc7037ebcd056e6d834e1
ljn <1368315257@qq.com>
2018-08-11 15:27:44 +0800

    Salary部分

---
+++ SalaryManagement/entity/Salary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryManagement.entity
+{
+    class Salary
+    {
+        //ABCD类课程单位课时工资
+        public float A { get; set; } = 26;
+        public float B { get; set; } = 25;
+        public float C { get; set; } = 23;
+        public float D { get; set; } = 20;
+        //早晚自习单位课时工资
+        public float EarlySelfStudy { get; set; } = 23;
+        public float EveningSelfStudy { get; set; } = 30;
+        public float Over { get; set; } = 10; //超课时补贴单位课时工资
+    }
+}
